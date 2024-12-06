d4f05dea10472c90f3b853f665c296373e25c925
Mikhail Nekrasov <nekrasovm2@gmail.com>
2024-12-06 15:27:23 +0200

    Fifth commit (digital service design project)

---
+++ WebGLEarthAPI.js
@@ -0,0 +1,28 @@
+var earth;
+var result = "";
+
+function initialize() {
+    var options = {
+        atmosphere: true,
+        center: [0, 0],
+        zoom: 0
+    };
+    earth = new WE.map('earth_div', options);
+  
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Â© OpenStreetMap contributors'
+    }).addTo(earth);
+
+}
+
+function goHaaga() {
+    earth.setView([60.201702584772704,24.934343647420686]);
+    earth.setZoom(17);
+    document.getElementById("result").innerHTML = "Haaga-Helia University of Applied Sciences, Pasila campus. <br> Address: Ratapihantie 13, 00520 Helsinki"
+}
+
+function goOodi() {
+    earth.setView([60.173657577064134,24.938366787461497]);
+    earth.setZoom(17);
+    document.getElementById("result").innerHTML = "Helsinki Central Library Oodi. <br> Address: Töölönlahdenkatu 4, 00100 Helsinki"
+}
